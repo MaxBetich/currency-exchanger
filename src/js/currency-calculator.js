1660a86c7e830a12d4bfa12f982afe33bd9db966
Max Betich <betichm@gmail.com>
2023-04-21 08:40:59 -0700

    add initial CurrencyExchanger static method

---
+++ src/js/currency-calculator.js
@@ -0,0 +1,17 @@
+export default class CurrencyExchanger {
+
+  static async getExchangeRates(value) {
+    let urlString = `https://v6.exchangerate-api.com/v6/${process.env.API_KEY}/latest/USD`;
+    try {
+      const response = await fetch(urlString);
+      const jsonifiedResponse = await response.json();
+      if (!response.ok) {
+        const errorMessage = `${response.status} ${response.statusText} ${jsonifiedResponse.message}`;
+        throw new Error(errorMessage);
+      }
+      return jsonifiedResponse;
+    } catch(error) {
+      return error;
+    }
+  }
+}
